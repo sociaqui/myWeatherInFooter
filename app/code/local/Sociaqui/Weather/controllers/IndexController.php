a2e052cdec727aae50bff9d9a20c29d0f508ffa7
Jarek Sochacki <sociaqui@gmail.com>
2018-09-10 19:34:01 +0200

    Initial commit

---
+++ app/code/local/Sociaqui/Weather/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+class Sociaqui_Weather_IndexController extends Mage_Core_Controller_Front_Action
+{
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+        echo 'Routing test.';
+    }
+
+}
