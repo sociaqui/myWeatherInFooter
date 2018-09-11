29157797cde7874300ef8b75b6356b22fcf84906
Jarek Sochacki <sociaqui@gmail.com>
2018-09-11 09:36:53 +0200

    Added retrieving data from API and displaying on Module index page

--- README.md
+++ README.md
@@ -6,9 +6,8 @@ A simple module to display some weather news in the footer of your Magento based
 
 A working Magento 1.9.3 install
 
-- [Magento](https://magento.com/tech-resources/download) 
-- Magento itself requires a working php installation, a web-server (i.e. nginx, apache), a working database, etc.
-- you can try [this](https://github.com/andreaskoch/dockerized-magento) dockerized version of ["Magento Community Edition 1.9.x"](https://github.com/andreaskoch/dockerized-magento) with a complete stack in multiple containers run with [docker-compose (1.7 or higher)](https://docs.docker.com/compose/).
+- [Magento](https://magento.com/tech-resources/download) itself requires a working php installation, a web-server (i.e. nginx, apache), a working database, etc.
+- you can try [this](https://github.com/andreaskoch/dockerized-magento) dockerized version of ["Magento Community Edition 1.9.x"](https://github.com/andreaskoch/dockerized-magento) with a complete stack in multiple containers run with [docker-compose](https://docs.docker.com/compose/) (1.7 or higher).
 
 ## Installation
 
@@ -17,8 +16,7 @@ A working Magento 1.9.3 install
 ## Usage
 
 TODO:
-- view on every page
-unless too modified
-also check cache
-- view grid in admin panel
+- add to footer to be viewed on every page (unless custom modified layouts - also check cache)
+- view previous data on grid in admin panel
+- basic admin setup fields
 - check data directly in database
--- app/code/local/Sociaqui/Weather/controllers/IndexController.php
+++ app/code/local/Sociaqui/Weather/controllers/IndexController.php
@@ -7,7 +7,86 @@ class Sociaqui_Weather_IndexController extends Mage_Core_Controller_Front_Action
      */
     public function indexAction()
     {
-        echo 'Routing test.';
+        $weather = $this->GetWeather();
+        
+        $output = 'RIGHT NOW THE WEATHER IN ' . $weather["LocationName"] . ' IS: ';
+        $output .= '<br>';
+        $output .= '<br>';
+        $output .= 'General conditions: ';
+        $output .= $weather["WeatherText"];
+        $output .= '<br>';
+        $output .= 'Temperature: ';
+        $output .= $weather['Temperature']['Metric']['Value'];
+        $output .= ' ';
+        $output .= $weather['Temperature']['Metric']['Unit'];
+        $output .= '<br>';
+        $output .= 'Wind: ';
+        $output .= $weather['Wind']['Speed']['Metric']['Value'];
+        $output .= ' ';
+        $output .= $weather['Wind']['Speed']['Metric']['Unit'];
+        $output .= ' ';
+        $output .= $weather['Wind']['Direction']['Localized'];
+        $output .= '<br>';
+        $output .= 'Pressure: ';
+        $output .= $weather['Pressure']['Metric']['Value'];
+        $output .= ' ';
+        $output .= $weather['Pressure']['Metric']['Unit'];
+        $output .= '<br>';
+        $output .= 'last updated: ';
+        $output .= $weather['LocalObservationDateTime'];
+        $output .= '<br>';
+        $output .= 'Source: ';
+        $output .= $weather['Link'];
+        $output .= '<br>';
+        $output .= '<img src="https://developer.accuweather.com/sites/default/files/' . sprintf("%02d", $weather["WeatherIcon"]) . '-s.png" width="75" height="45" alt="' . $weather["WeatherText"] . '" title="' . $weather["WeatherText"] . '">';
+
+        $this->getResponse()->setBody($output);
+
+    }
+
+    /**
+     * Gets the current weather from AccuWeather for the given numeric location code
+     * Lublin by default (use http://dataservice.accuweather.com/locations/v1/cities/search?apikey={key}&q={city}
+     * to find others
+     *
+     * @param int $locationCode
+     * @param string $location
+     * @param string $apiKey
+     * @return array
+     */
+    private function getWeather($locationCode = 274231, $location = 'Lublin', $apiKey = 'elbFGXDtVANeARtejGztyukgXomEIRBy')
+    {
+        $template = 'http://dataservice.accuweather.com/currentconditions/v1/%s.json?apikey=%s&details=true';
+        $url = sprintf($template, $locationCode, $apiKey);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_ENCODING => 'gzip,deflate',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_FAILONERROR => true,
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        $weather = [];
+
+        if (empty($err)) {
+            $weather = json_decode(utf8_encode($response), true)[0];
+        } else {
+            Mage::log('An error occurred while trying to access API endpoint', null, 'sociaqui_weather.log');
+            Mage::log('url: ' . $url, null, 'sociaqui_weather.log');
+            Mage::log('error: ' . $err, null, 'sociaqui_weather.log');
+        }
+
+        $weather["LocationName"] = $location;
+
+        return $weather;
     }
 
 }
